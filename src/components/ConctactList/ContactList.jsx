8ea413328ec59802631b9d5e21ba95fd5dc55ae7
Aleksandra <a.walczak1984@gmail.com>
2023-07-18 18:18:54 +0200

    next changes in components

--- src/components/ConctactList/ContactList.jsx
+++ src/components/ConctactList/ContactList.jsx
@@ -1,47 +1,24 @@
-// import React from 'react';
-// import { useDispatch } from 'react-redux';
-// import { deleteContact } from '../redux/contacts/contactsSlice';
-// import styles from './ContactList.module.css';
-
-// const ContactList = ({ contacts }) => {
-//   const dispatch = useDispatch();
-
-//   const handleDelete = contactId => {
-//     dispatch(deleteContact(contactId));
-//   };
-
-//   return (
-//     <ul className={styles.contactList}>
-//       {contacts.map(contact => (
-//         <li key={contact.id} className={styles.contactItem}>
-//           <span className={styles.contactName}>{contact.name}</span>
-//           <span className={styles.contactNumber}>{contact.number}</span>
-//           <button
-//             className={styles.deleteBtn}
-//             onClick={() => handleDelete(contact.id)}
-//           >
-//             Delete
-//           </button>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
-
-// export default ContactList;
-
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from '../redux/contacts/contactsSlice';
 import styles from './ContactList.module.css';
 
-const ContactList = ({ contacts, deleteContact }) => {
+const ContactList = ({ contacts }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = contactId => {
+    dispatch(deleteContact(contactId));
+  };
+
   return (
-    <ul>
+    <ul className={styles.contactList}>
       {contacts.map(contact => (
-        <li key={contact.id}>
-          {contact.name} - {contact.number}
+        <li key={contact.id} className={styles.contactItem}>
+          <span className={styles.contactName}>{contact.name}</span>
+          <span className={styles.contactNumber}>{contact.number}</span>
           <button
             className={styles.deleteBtn}
-            onClick={() => deleteContact(contact.id)}
+            onClick={() => handleDelete(contact.id)}
           >
             Delete
           </button>
@@ -52,3 +29,26 @@ const ContactList = ({ contacts, deleteContact }) => {
 };
 
 export default ContactList;
+
+// import React from 'react';
+// import styles from './ContactList.module.css';
+
+// const ContactList = ({ contacts, deleteContact }) => {
+//   return (
+//     <ul>
+//       {contacts.map(contact => (
+//         <li key={contact.id}>
+//           {contact.name} - {contact.number}
+//           <button
+//             className={styles.deleteBtn}
+//             onClick={() => deleteContact(contact.id)}
+//           >
+//             Delete
+//           </button>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
+
+// export default ContactList;
